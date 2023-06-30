bbdb08e39d5a35cea120555df1e97da603139be0
leleosilva <leo.hhcsilva@gmail.com>
2023-06-30 19:54:00 -0300

    Bisection method implemented

---
+++ root-finding/bisection.jl
@@ -0,0 +1,22 @@
+function bisection(f::Function, a::Number, b::Number, ξ::Real=1e-4, num_iter::Integer=100)
+    
+    # Initial check: whether a root exists in [a, b]
+    f(a)f(b) <= 0 || error("No real root in [a, b]")
+    
+    # Runs the method for 'num_iter' iterations 
+    for _ in 1:num_iter
+
+        x = (a + b) / 2
+
+        if f(x) == 0 || abs(b - a) < ξ
+            return x;
+        end
+
+        if f(a)f(x) > 0 # Root exists in the first half of [a, b]
+            a = x
+        else # Root exists in the second half of [a, b]
+            b = x
+        end
+    end
+    return Nothing # 
+end
